8270e558b4636be55bf81f3a285b2a525d78e937
takumakoike <takuma.151e@gmail.com>
2024-12-25 14:43:19 +0900

    #1 DB設計を実施

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), db()],
   adapter: vercel()
 });
